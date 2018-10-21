a86a4ae94b8846efd8094031af990722c4f23cda
Amber <adc351@gmail.com>
2018-10-21 14:32:29 -0700

    Checkpoint 7: done

--- src/components/Album.js
+++ src/components/Album.js
@@ -12,8 +12,38 @@ return album.slug===this.props.match.params.slug
 });
 	
 this.state = {
-album: album 
+album:album, 
+currentSong: album.songs[0], 
+isPlaying: false
 };
+
+this.audioElement= document.createElement('audio');
+this.audioElement.src=album.songs[0].audioSrc;
+}
+
+play() {
+this.audioElement.play(); 
+this.setState({isPlaying: true});
+}
+
+pause(){
+this.audioElement.pause();
+this.setState({isPlaying: false});
+}
+
+setSong(song){
+this.audioElement.src=song.audioSrc;
+this.setState({currentSong:song});
+}
+
+handleSongClick(song){
+const isSameSong=this.state.currentSong===song;
+if (this.state.isPlaying&& isSameSong) {
+this.pause();}
+else {
+if (!isSameSong) { this.setSong(song); }
+this.play();
+}
 }
 		
 render() {
@@ -35,7 +65,7 @@ return (
 </colgroup>  
 <tbody>
 {this.state.album.songs.map( (song, index) => 
-<tr className="song" key={index}>
+<tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
 <td className="song-actions">
 </td>
 <td className="song-number">{index+1}</td>
